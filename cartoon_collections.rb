e55954933c4fc540f5aea536f27599e65267900d
Monica Marziano <mmarziano1@gmail.com>
2018-04-04 11:10:53 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -16,8 +16,6 @@ end
 
 def find_the_cheese(food)
   cheese_types = ["cheddar", "gouda", "camembert"]
-  test_array = cheese_types & food
-    return test_array
   if food.sort == cheese_types.sort
     return true 
   end
